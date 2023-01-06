3e3fab6eb928684f72dd8143952c3c9c6a69464b
leonwebdev <109298601+leonwebdev@users.noreply.github.com>
2023-01-06 18:37:36 -0500

    Revised parallax parameters to get a best outcome on iPhone.

--- portfolio_gatsby/src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ portfolio_gatsby/src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -8,11 +8,11 @@ import Contact from "@lekoarts/gatsby-theme-cara/src/components/contact"
 
 const Cara = () => (
   <Layout>
-    <Parallax pages={5}>
+    <Parallax pages={6}>
       <Hero offset={0} factor={1} />
-      <Projects offset={1} factor={1} />
-      <About offset={2.9} factor={1.5} />
-      <Contact offset={4} factor={1} />
+      <Projects offset={1} factor={2} />
+      <About offset={3} factor={2} />
+      <Contact offset={5} factor={1} />
     </Parallax>
   </Layout>
 )
